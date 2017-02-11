cce8f6ae39c45d9304de0dffb6ddd04ae4a56f8b
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2017-02-11 01:16:58 -0200

    add unit tests for MinDaysBetweenActivities constraint (pre-gen)
    Signed-off-by: Rodolfo Ribeiro Gomes <rodolforg@gmail.com>

---
+++ tests/engine/GeneratePre/GeneratePre.pro
@@ -0,0 +1,78 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2017-02-08T22:04:11
+#
+#-------------------------------------------------
+
+QT       += testlib
+
+TARGET = tst_generatepretest
+CONFIG   += console testcase
+CONFIG   -= app_bundle
+CONFIG   += debug
+
+TEMPLATE = app
+
+# The following define makes your compiler emit warnings if you use
+# any feature of Qt which as been marked as deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# You can also select to disable deprecated APIs only up to a certain version of Qt.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+
+SOURCES += tst_generatepretest.cpp \
+    ../../../src/engine/generate_pre.cpp \
+    ../../../src/engine/activity.cpp \
+    ../../../src/engine/activitytag.cpp \
+    ../../../src/engine/building.cpp \
+    ../../../src/engine/lockunlock.cpp \
+    ../../../src/engine/room.cpp \
+    ../../../src/engine/rules.cpp \
+    ../../../src/engine/solution.cpp \
+    ../../../src/engine/spaceconstraint.cpp \
+    ../../../src/engine/studentsset.cpp \
+    ../../../src/engine/subject.cpp \
+    ../../../src/engine/teacher.cpp \
+    ../../../src/engine/timeconstraint.cpp \
+    ../../../src/engine/timetable_defs.cpp \
+    ../../../src/engine/xmllog.cpp \
+    ../../../src/engine/messageboxes.cpp \
+    ../../../src/engine/randomknuth.cpp \
+    ../../../src/interface/longtextmessagebox.cpp
+
+DEFINES += SRCDIR=\\\"$$PWD/\\\" FET_COMMAND_LINE
+
+HEADERS += \
+	../../../src/engine/activity.h \
+	../../../src/engine/solution.h \
+	../../../src/engine/timetable.h \
+	../../../src/engine/rules.h \
+	../../../src/engine/generate_pre.h \
+	../../../src/engine/timeconstraint.h \
+	../../../src/engine/spaceconstraint.h \
+	../../../src/engine/timetable_defs.h \
+	../../../src/engine/studentsset.h \
+	../../../src/engine/teacher.h \
+	../../../src/engine/subject.h \
+	../../../src/engine/activitytag.h \
+	../../../src/engine/room.h \
+	../../../src/engine/building.h \
+	../../../src/engine/lockunlock.h \
+#	engine/matrix.h \
+#	engine/randomknuth.h \
+	../../../src/engine/messageboxes.h \
+	../../../src/engine/xmllog.h \
+	\
+	../../../src/interface/longtextmessagebox.h
+
+OBJECTS_DIR = ../../../tmp/tests
+UI_DIR = ../../../tmp/tests
+MOC_DIR = ../../../tmp/tests
+RCC_DIR = ../../../tmp/tests
+
+INCLUDEPATH += $$PWD/../../../src/engine $$PWD/../../../src/interface
---
+++ tests/engine/GeneratePre/tst_generatepretest.cpp
@@ -0,0 +1,310 @@
+#include <QString>
+#include <QtTest>
+
+#include "generate_pre.h"
+
+Timetable gt;
+
+class GeneratePreTest : public QObject
+{
+	Q_OBJECT
+
+public:
+	GeneratePreTest();
+
+private:
+	struct MockRules3Activities
+	{
+		MockRules3Activities(Rules &rules);
+		~MockRules3Activities();
+	private:
+		Rules &rules;
+	};
+	Rules * rules;
+	void prepareMinDays();
+
+private slots:
+	void initTestCase();
+	void cleanupTestCase();
+	void MinDays_ReturnOkIfNoMinDaysConstraint();
+	void MinDays_InactiveConstraintIsIgnored();
+	void MinDays_ComputedSize();
+	void MinDays_CheckValues();
+	void MinDays_ActivityHasMoreThanOneOfThisConstraint();
+	void MinDays_FailIfActivityMinDaysToItself();
+	void MinDays_FailIfActivityMinDaysToItself_v2();
+	void MinDays_ThreeActivitiesAtOnce();
+};
+
+GeneratePreTest::GeneratePreTest()
+	: rules(NULL)
+{
+}
+
+void GeneratePreTest::prepareMinDays()
+{
+	minDaysListOfActivities.resize(rules->nInternalActivities);
+	minDaysListOfMinDays.resize(rules->nInternalActivities);
+	minDaysListOfConsecutiveIfSameDay.resize(rules->nInternalActivities);
+	minDaysListOfWeightPercentages.resize(rules->nInternalActivities);
+}
+
+void GeneratePreTest::initTestCase()
+{
+	VERBOSE = false;
+	rules = &gt.rules;
+}
+
+void GeneratePreTest::cleanupTestCase()
+{
+	rules->kill();
+}
+
+void GeneratePreTest::MinDays_ReturnOkIfNoMinDaysConstraint()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	bool result = computeMinDays(NULL);
+
+	QVERIFY2(result, "Could not compute MinDays constraint list");
+}
+
+void GeneratePreTest::MinDays_InactiveConstraintIsIgnored()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	QList<int> acts;
+	acts << 12345 << 23456;
+	ConstraintMinDaysBetweenActivities *ctr = new ConstraintMinDaysBetweenActivities(50.0, false, 2, acts, 5);
+	rules->addTimeConstraint(ctr);
+	ctr->active = false;
+	rules->computeInternalStructure(NULL);
+
+	bool result = computeMinDays(NULL);
+
+	QVERIFY2(result, "Could not compute MinDays constraint list");
+
+	QCOMPARE(minDaysListOfActivities.getD1(), 3);
+
+	QCOMPARE(minDaysListOfActivities[0].count(), 0);
+	QCOMPARE(minDaysListOfMinDays[0].count(), 0);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[0].count(), 0);
+	QCOMPARE(minDaysListOfWeightPercentages[0].count(), 0);
+
+	QCOMPARE(minDaysListOfActivities[1].count(), 0);
+	QCOMPARE(minDaysListOfMinDays[1].count(), 0);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[1].count(), 0);
+	QCOMPARE(minDaysListOfWeightPercentages[1].count(), 0);
+
+}
+
+void GeneratePreTest::MinDays_ComputedSize()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	QList<int> acts;
+	acts << 12345 << 23456;
+	ConstraintMinDaysBetweenActivities *ctr = new ConstraintMinDaysBetweenActivities(50.0, false, 2, acts, 5);
+	rules->addTimeConstraint(ctr);
+	rules->computeInternalStructure(NULL);
+
+	bool result = computeMinDays(NULL);
+
+	QVERIFY2(result, "Could not compute MinDays constraint list");
+
+	QCOMPARE(minDaysListOfActivities.getD1(), 3);
+
+	QCOMPARE(minDaysListOfActivities[0].count(), 1);
+	QCOMPARE(minDaysListOfMinDays[0].count(), 1);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[0].count(), 1);
+	QCOMPARE(minDaysListOfWeightPercentages[0].count(), 1);
+
+	QCOMPARE(minDaysListOfActivities[1].count(), 1);
+	QCOMPARE(minDaysListOfMinDays[1].count(), 1);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[1].count(), 1);
+	QCOMPARE(minDaysListOfWeightPercentages[1].count(), 1);
+}
+
+void GeneratePreTest::MinDays_CheckValues()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	QList<int> acts;
+	acts << 12345 << 23456;
+	ConstraintMinDaysBetweenActivities *ctr = new ConstraintMinDaysBetweenActivities(50.0, false, 2, acts, 5);
+	rules->addTimeConstraint(ctr);
+	rules->computeInternalStructure(NULL);
+
+	bool result = computeMinDays(NULL);
+
+	QVERIFY2(result, "Could not compute MinDays constraint list");
+
+	QCOMPARE(minDaysListOfActivities.getD1(), 3);
+
+	QCOMPARE(minDaysListOfActivities[0][0], rules->activitiesHash.value(23456, -1));
+	QCOMPARE(minDaysListOfMinDays[0][0], 5);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[0][0], false);
+	QCOMPARE(minDaysListOfWeightPercentages[0][0], 50.0);
+
+	QCOMPARE(minDaysListOfActivities[1][0], rules->activitiesHash.value(12345, -1));
+	QCOMPARE(minDaysListOfMinDays[1][0], 5);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[1][0], false);
+	QCOMPARE(minDaysListOfWeightPercentages[1][0], 50.0);
+}
+
+void GeneratePreTest::MinDays_ActivityHasMoreThanOneOfThisConstraint()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	QList<int> acts;
+	acts << 12345 << 23456;
+	ConstraintMinDaysBetweenActivities *ctr1 = new ConstraintMinDaysBetweenActivities(50.0, false, 2, acts, 5);
+	rules->addTimeConstraint(ctr1);
+	acts.clear();
+	acts << 23456 << 34567;
+	ConstraintMinDaysBetweenActivities *ctr2 = new ConstraintMinDaysBetweenActivities(75.0, true, 2, acts, 4);
+	rules->addTimeConstraint(ctr2);
+	rules->computeInternalStructure(NULL);
+
+	bool result = computeMinDays(NULL);
+
+	QVERIFY2(result, "Could not compute MinDays constraint list");
+
+	QCOMPARE(minDaysListOfActivities.getD1(), 3);
+
+	QCOMPARE(minDaysListOfActivities[1].count(), 2);
+	QCOMPARE(minDaysListOfMinDays[1].count(), 2);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[1].count(), 2);
+	QCOMPARE(minDaysListOfWeightPercentages[1].count(), 2);
+
+	QCOMPARE(minDaysListOfActivities[2].count(), 1);
+	QCOMPARE(minDaysListOfMinDays[2].count(), 1);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[2].count(), 1);
+	QCOMPARE(minDaysListOfWeightPercentages[2].count(), 1);
+
+	QCOMPARE(minDaysListOfActivities[2][0], rules->activitiesHash.value(23456, -1));
+	QCOMPARE(minDaysListOfMinDays[2][0], 4);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[2][0], true);
+	QCOMPARE(minDaysListOfWeightPercentages[2][0], 75.0);
+}
+
+void GeneratePreTest::MinDays_FailIfActivityMinDaysToItself()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	QList<int> acts;
+	acts << 12345 << 12345;
+
+	QSKIP("Disable test due to usage of assert in Rules::addTimeConstraint()");
+
+	ConstraintMinDaysBetweenActivities *ctr1 = new ConstraintMinDaysBetweenActivities(80.0, false, 2, acts, 5);
+	rules->addTimeConstraint(ctr1);
+	rules->computeInternalStructure(NULL);
+
+	bool result = computeMinDays(NULL);
+	QVERIFY2(result == false, "Should not accept constraint MinDaysBetweenActivities if the activities are the same one");
+}
+
+void GeneratePreTest::MinDays_FailIfActivityMinDaysToItself_v2()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	QList<int> acts;
+	acts << 12345 << 12345;
+	ConstraintMinDaysBetweenActivities *ctr1 = new ConstraintMinDaysBetweenActivities(80.0, false, 2, acts, 5);
+	rules->timeConstraintsList.append(ctr1);
+	rules->computeInternalStructure(NULL);
+
+	bool result = computeMinDays(NULL);
+	QVERIFY2(result == false, "Should not accept constraint MinDaysBetweenActivities if the activities are the same one");
+}
+
+void GeneratePreTest::MinDays_ThreeActivitiesAtOnce()
+{
+	MockRules3Activities mock(*rules);
+	prepareMinDays();
+
+	QList<int> acts;
+	acts << 12345 << 23456 << 34567;
+	ConstraintMinDaysBetweenActivities *ctr1 = new ConstraintMinDaysBetweenActivities(80.0, true, 3, acts, 2);
+	rules->timeConstraintsList.append(ctr1);
+	rules->computeInternalStructure(NULL);
+
+	bool result = computeMinDays(NULL);
+
+	QVERIFY2(result, "Could not compute MinDays constraint list");
+
+	QCOMPARE(minDaysListOfActivities.getD1(), 3);
+
+	QCOMPARE(minDaysListOfActivities[0].count(), 2);
+
+	QCOMPARE(minDaysListOfActivities[0][0], rules->activitiesHash.value(23456, -1));
+	QCOMPARE(minDaysListOfMinDays[0][0], 2);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[0][0], true);
+	QCOMPARE(minDaysListOfWeightPercentages[0][0], 80.0);
+
+	QCOMPARE(minDaysListOfActivities[0][1], rules->activitiesHash.value(34567, -1));
+	QCOMPARE(minDaysListOfMinDays[0][1], 2);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[0][1], true);
+	QCOMPARE(minDaysListOfWeightPercentages[0][1], 80.0);
+
+	QCOMPARE(minDaysListOfActivities[1].count(), 2);
+
+	QCOMPARE(minDaysListOfActivities[1][0], rules->activitiesHash.value(12345, -1));
+	QCOMPARE(minDaysListOfMinDays[1][0], 2);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[1][0], true);
+	QCOMPARE(minDaysListOfWeightPercentages[1][0], 80.0);
+
+	QCOMPARE(minDaysListOfActivities[1][1], rules->activitiesHash.value(34567, -1));
+	QCOMPARE(minDaysListOfMinDays[1][1], 2);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[1][1], true);
+	QCOMPARE(minDaysListOfWeightPercentages[1][1], 80.0);
+
+	QCOMPARE(minDaysListOfActivities[2].count(), 2);
+
+	QCOMPARE(minDaysListOfActivities[2][0], rules->activitiesHash.value(12345, -1));
+	QCOMPARE(minDaysListOfMinDays[2][0], 2);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[2][0], true);
+	QCOMPARE(minDaysListOfWeightPercentages[2][0], 80.0);
+
+	QCOMPARE(minDaysListOfActivities[2][1], rules->activitiesHash.value(23456, -1));
+	QCOMPARE(minDaysListOfMinDays[2][1], 2);
+	QCOMPARE(minDaysListOfConsecutiveIfSameDay[2][1], true);
+	QCOMPARE(minDaysListOfWeightPercentages[2][1], 80.0);
+}
+
+GeneratePreTest::MockRules3Activities::MockRules3Activities(Rules &_rules)
+	: rules(_rules)
+{
+	QStringList teachers;
+	QStringList activitytags;
+	QStringList studentsnames;
+	rules.init();
+	Subject *subject = new Subject();
+	subject->name = "Subject1";
+	rules.addSubjectFast(subject);
+	Subject *subject2 = new Subject();
+	subject2->name = "Subject2";
+	rules.addSubjectFast(subject2);
+	//Activity *act = new Activity(1, 1, teachers, "Subject", activitytags, studentsnames, 1, 1, true, true, -1, -1);
+	rules.addSimpleActivityFast(NULL, 12345, 12345, teachers, "Subject1", activitytags, studentsnames, 1, 1, true, false, 10, 10);
+	rules.addSimpleActivityFast(NULL, 23456, 23456, teachers, "Subject2", activitytags, studentsnames, 1, 1, true, false, 10, 10);
+	rules.addSimpleActivityFast(NULL, 34567, 34567, QStringList(), "Subject1", QStringList(), QStringList(), 1, 1, true, false, 10, 10);
+	rules.computeInternalStructure(NULL);
+}
+
+GeneratePreTest::MockRules3Activities::~MockRules3Activities()
+{
+	rules.kill();
+}
+
+QTEST_APPLESS_MAIN(GeneratePreTest)
+
+#include "tst_generatepretest.moc"
