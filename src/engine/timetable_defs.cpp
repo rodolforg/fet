2a899d5d9f3671cf8a9903ae11b063152409ba2f
Liviu Lalescu <liviu2@lalescu.ro>
2017-12-29 11:11:31 -0200

    version bumping 5.35.0 snapshot 2017.12.29-15:10

--- src/engine/timetable_defs.cpp
+++ src/engine/timetable_defs.cpp
@@ -34,7 +34,7 @@ QString internetVersion;
 /**
 FET version
 */
-const QString FET_VERSION="5.35.0-snapshot-28-dec-2017-18_31";
+const QString FET_VERSION="5.35.0-snapshot-29-dec-2017-15_10";
 
 /**
 FET language
