61965e75ed061af234a3704772db400b62f79a78
Liviu Lalescu <liviu2@lalescu.ro>
2018-01-12 16:03:39 -0200

    version bumping 5.35.1 spanshot 2018.01.12-20:02

--- src/engine/timetable_defs.cpp
+++ src/engine/timetable_defs.cpp
@@ -34,7 +34,7 @@ QString internetVersion;
 /**
 FET version
 */
-const QString FET_VERSION="5.35.1-snapshot-12-jan-2018-15_40";
+const QString FET_VERSION="5.35.1-snapshot-12-jan-2018-20_02";
 
 /**
 FET language
