38e8aeed357335142b20bfe73abbf25844430295
Rodolfo Ribeiro Gomes <rodolforg@gmail.com>
2018-04-29 14:41:45 -0300

    LockUnlock now has methods for locking, unlocking, and checking
    Signed-off-by: Rodolfo Ribeiro Gomes <rodolforg@gmail.com>

--- src/engine/lockunlock.h
+++ src/engine/lockunlock.h
@@ -25,6 +25,11 @@ File lockunlock.h
 #define LOCKUNLOCK_H
 
 #include <QObject>
+#include "errorcode.h"
+
+class Rules;
+class ConstraintActivityPreferredStartingTime;
+class ConstraintActivityPreferredRoom;
 
 class CommunicationSpinBox: public QObject{
 	Q_OBJECT
@@ -45,7 +50,39 @@ public:
 	static void computeLockedUnlockedActivitiesTimeSpace();
 	static void computeLockedUnlockedActivitiesOnlyTime();
 	static void computeLockedUnlockedActivitiesOnlySpace();
-	
+
+	/// Reports whether an activity has any kind of time lock
+	static bool isActivityTimeLocked(int activityId);
+	/// Reports whether an activity time is permanently locked
+	static bool isActivityTimePermLocked(int activityId);
+
+	/// Reports whether an activity has any kind of space lock
+	static bool isActivitySpaceLocked(int activityId);
+	/// Reports whether an activity space is permanently locked
+	static bool isActivitySpacePermLocked(int activityId);
+
+	/// Return a not-NULL pointer if activity time-lock is successfully added (not repeated)
+	static ConstraintActivityPreferredStartingTime* lockTime(Rules* rules, int activityId, int day, int hour);
+
+	/** Remove a time-lock for an activity.
+	 * @param[in] rules The timetable rules
+	 * @param[in] activityId The activity id
+	 * @param[out] nUnlocked The number of undone locks. There should be only one time-lock per activityID, but...
+	 * @return list of errors or messages. @see ErrorList::hasError() and ErrorRenderer
+	 */
+	static ErrorList unlockTime(Rules* rules, int activityId, int& nUnlocked);
+
+	/// Return a not-NULL pointer if activity space-lock is successfully added (not repeated)
+	static ConstraintActivityPreferredRoom* lockSpace(Rules* rules, int activityId, const QString& roomName);
+
+	/** Remove a space-lock for an activity.
+	 * @param[in] rules The timetable rules
+	 * @param[in] activityId The activity id
+	 * @param[out] nUnlocked The number of undone locks. There should be only one space-lock per activityID, but...
+	 * @return list of errors or messages. @see ErrorList::hasError() and ErrorRenderer
+	 */
+	static ErrorList unlockSpace(Rules* rules, int activityId, int& nUnlocked);
+
 	static void increaseCommunicationSpinBox();
 
 	static QString getActivityLockTipString(int activityId);
