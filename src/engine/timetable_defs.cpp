dafeffd82ab81195321e769ef2ce470f6c2aca6e
Liviu Lalescu <liviu2@lalescu.ro>
2018-06-10 06:31:54 -0300

    version bumping 5.36.0 snapshot 2018.06.10-12:31

--- src/engine/timetable_defs.cpp
+++ src/engine/timetable_defs.cpp
@@ -36,7 +36,7 @@ int STUDENTS_COMBO_BOXES_STYLE=STUDENTS_COMBO_BOXES_STYLE_SIMPLE;
 /**
 FET version
 */
-const QString FET_VERSION="5.36.0-snapshot-10-jun-2018-00_42";
+const QString FET_VERSION="5.36.0-snapshot-10-jun-2018-12_31";
 
 /**
 FET language
