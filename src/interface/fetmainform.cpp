d518f2e3506b04ca9d7080727b9266ce49fcce8b
Liviu Lalescu <liviu2@lalescu.ro>
2018-12-21 10:25:49 -0300

    add some privacy politics notice

--- src/interface/fetmainform.cpp
+++ src/interface/fetmainform.cpp
@@ -826,6 +826,8 @@ void FetMainForm::checkForUpdatesToggled(bool checked)
 			"request for this file will be visible on the server, along with your IP address and access time.")
 			.arg("https://lalescu.ro/liviu/fet/crtversion/crtversion.txt");
 		s+=" ";
+		s+=tr("Also, there will be visible on the server your current FET version and your operating system name and version.");
+		s+=" ";
 		s+=tr("Thus, it could be deduced if and when you use FET.");
 		s+="\n\n";
 		s+=tr("Do you agree?");
